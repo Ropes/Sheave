5e05355c13151e2e5fa334c643dc900b2901f596
Josh Roppo <joshroppo@gmail.com>
2014-07-03 15:59:29 -0700

    Attempting to get bot listening to channels

--- sheave.go
+++ sheave.go
@@ -8,9 +8,21 @@ import (
 
 //var ircc = &client.IRCClient{Nickname: "sheave"}
 
+func GopherHandler(event *irc.Event) {
+	client := event.Client
+
+	switch event.Command {
+	case "!nextmeetup":
+		fmt.Println("EventAgs: ", event.Arguments)
+		client.SendRawf("Hack Night 2014/7/9",
+			event.Arguments[len(event.Arguments)-1])
+	}
+}
+
 func main() {
 	ircc := irc.New("sheave", "sheave")
 	ircc.Server = "chat.freenode.net:6667"
+	ircc.RealName = "PDX Gopher"
 
 	/*
 		var ircc = &irc.IRCClient{
@@ -21,11 +33,14 @@ func main() {
 			//heartbeatInterval: 1,
 		}
 	*/
+	fmt.Println(ircc)
 	connErr := ircc.Connect("chat.freenode.net:6667")
-	ircc.Join("pdxgo")
 	if connErr != nil {
 		fmt.Println("Connection Error: \n", connErr)
 	}
+
+	ircc.Join("#pdxgo")
+	ircc.AddHandler(GopherHandler)
 	ircc.Run()
 
 }

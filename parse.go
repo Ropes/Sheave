d41db1b9048567d9056fc47c16f4d53056b56df9
Josh Roppo <joshroppo@gmail.com>
2014-07-14 11:42:14 -0700

    Updating JSON parser, JSON, and Event struct
    Applying changes to JSON parsing and Event data model to parse.go and JSON
    files.

--- parse.go
+++ parse.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"time"
 )
 
 type IRCConfig struct {
@@ -29,6 +30,34 @@ func parseConfig(path string) IRCConfig {
 
 type Hacknights struct {
 	data map[string]interface{}
+}
+
+type Event struct {
+	Time        time.Time
+	Localtime   string
+	Location    string
+	Lat         float64
+	Lon         float64
+	Link        string
+	Tweet       string
+	Topic       string
+	Description []string
+	Notes       string
+}
+
+func parseEvent(path string, e chan Event) {
+	contents, err := ioutil.ReadFile(path)
+	var cal Event
+	if err != nil {
+		fmt.Println(err)
+		e <- cal
+	}
+	err = json.Unmarshal(contents, &cal)
+	if err != nil {
+		fmt.Println(err)
+		e <- cal
+	}
+	e <- cal
 }
 
 func parseCalendar(path string, e chan interface{}) {
--- sheave_test.go
+++ sheave_test.go
@@ -1,12 +1,6 @@
 package sheave
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"testing"
-	"time"
-)
+import "testing"
 
 func TestLocalEventParsing(t *testing.T) {
 	LoadCalendar()
@@ -15,37 +9,11 @@ func TestLocalEventParsing(t *testing.T) {
 	}
 }
 
-type Event struct {
-	Time        time.Time
-	Localtime   string
-	Location    string
-	Lat         float64
-	Lon         float64
-	Topic       string
-	Description []string
-	Notes       string
-}
-
-func parseCal(path string, e chan Event) {
-	contents, err := ioutil.ReadFile(path)
-	var cal Event
-	if err != nil {
-		fmt.Println(err)
-		e <- cal
-	}
-	err = json.Unmarshal(contents, &cal)
-	if err != nil {
-		fmt.Println(err)
-		e <- cal
-	}
-	e <- cal
-}
-
 func TestTalkTargetEventParsing(t *testing.T) {
 	c := make(chan Event)
-	go parseCal("testing/resources/talking.json", c)
+	go parseEvent("testing/resources/talking.json", c)
 	jsn := <-c
-	fmt.Printf("%#v\n", jsn)
+	//fmt.Printf("%#v\n", jsn)
 	if jsn.Location != "ESRI" {
 		t.Errorf("Location incorrect: %#v", jsn)
 	}

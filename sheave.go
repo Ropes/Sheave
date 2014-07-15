86c3c2490e8ec87ba0b120050e1d4c95151b9558
Josh Roppo <joshroppo@gmail.com>
2014-07-15 13:19:36 -0700

    Abstracted sending multiple messages to a channel
    SendPrivMsgs will send multiple messages([]string) to a given channel
    via an irc.Event struct.  Funciton iterates through messages and sends
    each via PRIVMSGs.

--- sheave.go
+++ sheave.go
@@ -23,6 +23,8 @@ func LoadCalendar() {
 	events.hacknight, events.talknight = <-c, <-c
 }
 
+//EventResponse creates a []string of useful information of an Event(struct)
+//which will be sent to inquiring user.
 func EventResponse(e Event, user string, etype string) []string {
 	resp := make([]string, 0)
 	msg := fmt.Sprintf("%s: Next %s: %s", user, etype, e.Localtime)
@@ -34,6 +36,15 @@ func EventResponse(e Event, user string, etype string) []string {
 	msg = fmt.Sprintf("Info: %s", e.Link)
 	resp = append(resp, msg)
 	return resp
+}
+
+//SendPrivMsgs broadcasts PRIVMSGs via the given client and channel.
+//msgs []string; are the messages to be sent
+func SendPrivMsgs(event *irc.Event, channel string, msgs []string) {
+	client := event.Client
+	for _, msg := range msgs {
+		client.Privmsg(channel, msg)
+	}
 }
 
 //GopherHandler which responds with the next meeting type for the !nextmeetup command
@@ -51,32 +62,16 @@ func GopherHandler(event *irc.Event) {
 			log.Printf("Channel: %+v", channel)
 			switch cmd {
 			case "!nextmeetup":
-				msg := fmt.Sprintf("%s: %s", user, "meetingtime!")
+				msg := fmt.Sprintf("%s: %s", user, " TODO: meetingtime!")
 				client.Privmsg(channel, msg)
-
 			case "!nexttalk":
-
-				msg := fmt.Sprintf("%s: Next Talk night: %s", user, events.talknight.Localtime)
-				client.Privmsg(channel, msg)
-
-				msg = fmt.Sprintf(">>> %s @ %s <<<", events.talknight.Topic, events.talknight.Location)
-				client.Privmsg(channel, msg)
-
-				msg = fmt.Sprintf("Info: %s", events.talknight.Link)
-				client.Privmsg(channel, msg)
-
+				msgs := EventResponse(events.talknight, user, "Talk Night")
+				SendPrivMsgs(event, channel, msgs)
 			case "!nexthack":
-				msg := fmt.Sprintf("%s: Next Hack Night: %s", user, events.hacknight.Localtime)
-				client.Privmsg(channel, msg)
-
-				msg = fmt.Sprintf(">>> %s @ %s <<<", events.hacknight.Topic, events.hacknight.Location)
-				client.Privmsg(channel, msg)
-
-				msg = fmt.Sprintf("Info: %s", events.hacknight.Link)
-				client.Privmsg(channel, msg)
+				msgs := EventResponse(events.hacknight, user, "Hack Night")
+				SendPrivMsgs(event, channel, msgs)
 			case "!sheavehelp":
 				client.Privmsg(channel, "Sheavebot Cmds: !nextmeetup !nexttalk !nexthack")
-
 			}
 		}
 	}

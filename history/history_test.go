655d2a7ff7317f642e9e9e71926333e547353e5f
Josh Roppo <joshroppo@gmail.com>
2014-07-27 00:46:48 -0700

    Tests passing and checking for insertion order

--- history/history_test.go
+++ history/history_test.go
@@ -6,17 +6,26 @@ import (
 	"testing"
 )
 
+var x []string
+var a []string
+
 func TestInit(t *testing.T) {
-	x := []string{"x", "y", "z"}
-	a := []string{"a", "b", "c"}
-	fmt.Println(x, a)
+	x = []string{"x", "y", "z"}
+	a = []string{"a", "b", "c"}
+	//fmt.Println(x, a)
 
 	hh := make(HistoryHeap, 20)
 	heap.Push(&hh, x)
+	if hh[0][0] != "x" {
+		t.Errorf("First element incorrect: %#v", hh[0][0])
+	}
 	heap.Push(&hh, a)
 
 	fmt.Printf("hh: %#v\n", hh)
 	if hh == nil {
 		t.Errorf("First history stored is: %#v", hh)
+	}
+	if hh[0][0] != "a" {
+		t.Errorf("First element incorrect: %#v", hh[0][0])
 	}
 }
